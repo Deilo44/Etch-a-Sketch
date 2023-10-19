6c2db819ec73f3d878c2e0bdecf58816a5216892
danokirror@gmail.com <danokirror@gmail.com>
2023-10-19 12:39:45 +0300

    working on the code for changing the board size

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns ="repeat(16 , 1fr)";
-board.style.gridTemplateRow ="repeat(16 , 1fr)";
+function creatediv(size){
+    let board = document.querySelector(".board");
+    let squares =board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns =`repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows =`repeat(${size} , 1fr)`;
+    let number = size*size;
+    for (let i=0;i<number;i++){
+        let square = document.createElement("div");
+        square.style.backgroundColor="antiquewhite";
+        board.insertAdjacentElement("beforeend",square);
+} 
+}
 
-for (let i=0;i<256;i++){
-    let square = document.createElement("div");
-    square.style.backgroundColor="blue";
-    board.insertAdjacentElement("beforeend",square);
+creatediv(16);
+
+function boardSize(input){
+    creatediv(input);
 }
